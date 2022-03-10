40e16c3e89badd7209623f6491a0528e0ef90f76
rafael gonzalez <rafaelg@indeed.com>
2022-03-10 20:55:46 +0000

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 RestAPI
+
+python -m venv venv
+
+always activate vitual environment
+
+cd to venv/Scripts/activate
+
+pip install -r requirements.txt
---
+++ backend/api/views.py
@@ -0,0 +1,16 @@
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.models import Product
+from products.serializers import ProductSerializer
+
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        #data = model_to_dict(model_data, fields=['id', 'title','price', 'sale_price'])
+        data = ProductSerializer(instance).data
+        
+    return Response(data)
